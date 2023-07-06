31ce6fe8a3ce43af5f38548c45494c4fa7b5a029
AmritDizzy <86696470+AmritDizzy@users.noreply.github.com>
2023-07-06 16:14:26 -0400

    Delete menu.php

--- menu.php
+++
@@ -1,7 +0,0 @@
- <?php include('database.php')?>
- <div class="navigation"  style="background-color: orange;" >
-       <ul>
-           <li><a href="index.php" class="home">Home</a></li>
-           <li><a href="contact.php">Contact Us</a></li>
-	   </ul>
-  </div>
