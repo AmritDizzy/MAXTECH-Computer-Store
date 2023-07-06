e5606532fb88eead422bcfede76eeb80806de54e
AmritDizzy <86696470+AmritDizzy@users.noreply.github.com>
2023-07-06 16:19:23 -0400

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,34 @@
+
+<footer id="footer">
+		<div class="cols">
+				<div class="col media">
+				  
+					<h3>Social media</h3>
+					<ul class="social">
+						<li><a href="#"><span class="ico ico-fb"></span>Facebook</a></li>
+						<li><a href="#"><span class="ico ico-tw"></span>Twitter</a></li>
+						<li><a href="#"><span class="ico ico-gp"></span>Google+</a></li>
+						<li><a href="#"><span class="ico ico-pi"></span>Pinterest</a></li>
+					</ul>
+				
+				</div>
+				<div class="col contact">
+				
+					<h3>Contact us</h3>
+					<p>MAXTECH COMPUTERS<br>New Market<br>Chaguanas</p>
+					<p><span class="ico ico-em"></span><a href="#">contact@maxtechcomputers.com</a></p>
+					<p><span class="ico ico-ph"></span>(+868) 7864013</p>
+				</div>
+				<div class="col payment">
+					<h3>Payment System</h3>
+					<img src="images/cash.png">OR
+					<img src="images/endcash.png">
+					
+					</form>
+				</div>
+		</div>
+			<center><p class="copy">Copyright 2023 . All rights reserved.</p></center>
+		
+		<!-- / container -->
+	</footer>
+	
