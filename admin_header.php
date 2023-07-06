e945d2b832c17936d420105fd2d09c3b9a46067b
AmritDizzy <86696470+AmritDizzy@users.noreply.github.com>
2023-07-06 15:57:40 -0400

    Add files via upload

---
+++ admin_header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<title>MAXTECH COMPUTERS</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" media="all" href="css/admin.css">
+	<script src="js/jquery.js"></script>  
+    <script src="js/main.js"></script> 
+	<link rel="shortcut icon" href="images/logo/cart.png" type="image/x-icon">
+
+	
+</head>
+
+<body>
+    <header>
+	   <div id="admin-header"   >
+			<div class="header">
+			
+
+			<h2 >  <img src="Maxtech-Logo.jpg" class="header_title"> <br> &nbsp;&nbsp;&nbsp; MAXTECH COMPUTERS    &nbsp;   (EMPLOYEE DASHBOARD)  &nbsp;</h2>
+
+			</div>
+
+
+
+
+         
+       </div>
+   </header>
+
+
